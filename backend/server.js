67f592218497a08a01c9cda43be9208e5662c933
Harshithsoma <somaharshith2000@gmail.com>
2024-08-13 15:37:04 -0700

    deployment setup done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -9,14 +9,21 @@ import messageRoute from "./routes/message.route.js";
 import { connectToMongoDB } from "./db/dbauth.js";
 import { app, server } from "./socket/socket.js";
 // const app = express();
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/users", userRoute);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 const port = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.send("Chat application development is in progress");
 });
